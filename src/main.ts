21f80b894f8262d4535fffe71ac45eddd50e4ed6
Hiago <hiagofss98@gmail.com>
2024-07-17 22:51:28 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('MS Doctors')
+    .setDescription('Microservices to manage doctors')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333, '0.0.0.0');
 }
 bootstrap();
