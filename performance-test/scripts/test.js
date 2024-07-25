a92985f0f0fd9c778ea6266115fc3c19cb07705d
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-07-25 13:18:42 -0300

    Update test.js

--- performance-test/scripts/test.js
+++ performance-test/scripts/test.js
@@ -1,16 +1,16 @@
-import http from 'k6/http';
-import { check } from 'k6';
+// import http from 'k6/http';
+// import { check } from 'k6';
 
-// Test configuration
-export const options = {
-  thresholds: {
-    http_req_duration: ['p(99) < 5000'],
-  },
-  stages: [{ duration: '30s', target: 5000 }],
-};
+// // Test configuration
+// export const options = {
+//   thresholds: {
+//     http_req_duration: ['p(99) < 5000'],
+//   },
+//   stages: [{ duration: '30s', target: 5000 }],
+// };
 
-// Simulated user behavior
-export default function () {
-  let req = http.get('http://host.docker.internal:3333/');
-  check(req, { 'status was 200': (r) => r.status === 200 });
-}
+// // Simulated user behavior
+// export default function () {
+//   let req = http.get('http://host.docker.internal:3333/');
+//   check(req, { 'status was 200': (r) => r.status === 200 });
+// }
